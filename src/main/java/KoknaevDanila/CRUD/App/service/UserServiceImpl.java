fa3a403077d000e03a822b26ccad3605fba1ef19
DanilaKoknaev <danilakoknaev@gmail.com>
2025-08-20 18:22:23 +0300

    Транзакции добавил

--- src/main/java/KoknaevDanila/CRUD/App/service/UserServiceImpl.java
+++ src/main/java/KoknaevDanila/CRUD/App/service/UserServiceImpl.java
@@ -12,26 +12,31 @@ import java.util.List;
 public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
 
+    @Autowired
     public UserServiceImpl(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
 
     @Override
+    @Transactional (readOnly = true)
     public List<User> getAllUsers() {
         return userRepository.findAll();
     }
 
     @Override
+    @Transactional
     public void saveUser(User user) {
         userRepository.save(user);
     }
 
     @Override
+    @Transactional (readOnly = true)
     public User getUser(long id) {
         return userRepository.findById(id).orElse(null);
     }
 
     @Override
+    @Transactional
     public void deleteUser(long id) {
         userRepository.deleteById(id);
     }
